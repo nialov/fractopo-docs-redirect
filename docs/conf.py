2756e415a299e1663c247ac4812294b6433f06ad
nialov <nikolasovaskainen@gmail.com>
2026-04-16 11:06:52 +0300

    Initial tombstone docs: Sphinx redirect to GitHub Pages, RTD config, requirements, and robots.txt

---
+++ docs/conf.py
@@ -0,0 +1,8 @@
+extensions = ['sphinx_reredirects']
+# Redirect every page to the new home
+redirects = {
+    "**": "https://nialov.github.io/fractopo/"
+}
+html_baseurl = "https://nialov.github.io/fractopo/"
+html_show_sourcelink = False
+html_copy_source = False
---
+++ docs/index.rst
@@ -0,0 +1,8 @@
+fractopo documentation has moved!
+=====================================
+
+The documentation for fractopo is now hosted at:
+
+    https://nialov.github.io/fractopo/
+
+You will be redirected shortly. If not, please click the link above.
